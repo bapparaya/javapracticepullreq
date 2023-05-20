a9a2f917ede4799313d9152cf934804ceba5933c
bapparaya <boppamuthuraj78605@gmail.com>
2023-05-20 10:57:05 +0530

    inheritance

---
+++ src/javapracticepullreq/Inheritance.java
@@ -0,0 +1,23 @@
+package javapracticepullreq;
+
+class A{
+	
+	int num=10;
+	
+	public void display() {
+		System.out.println(num);
+	}
+}
+
+
+public class Inheritance extends A {
+
+	public static void main(String[] args) {
+		
+		Inheritance obj= new Inheritance();
+		obj.display();
+		
+
+	}
+
+}
